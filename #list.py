4cace7ceb009b4ac00ac1d066411e33e590d56f4
Sopheak Saing <101089099+Pheak02@users.noreply.github.com>
2022-12-08 11:25:19 +0700

    Add files via upload

---
+++ #list.py
@@ -0,0 +1,15 @@
+#list
+Friends =["sopheak", "many", "mony", "dalin", "dano"]
+#print all
+print(Friends)
+#print by index
+print(Friends[0]) #the first one
+print(Friends[-1]) # print from right to left, index start with -1
+print(Friends[1:]) #will output the element from 1
+print(Friends[1:3]) #will output from index 1 until 2
+
+#extend the list
+numbers =[2,5,7,4,2]
+Friends =["sopheak", "many", "mony", "dalin", "dano"]
+Friends.extend(numbers)
+print(Friends)
